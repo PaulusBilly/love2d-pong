90beb4a2aac46e4a406175ed93d4f53bbee1a942
Billy <141384133+PaulusBilly@users.noreply.github.com>
2026-05-06 16:20:06 +0700

    added start state & reset functionality, added start state text, encapsulate the ball spawn function

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@ local push = require 'push'
 function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
 
+  -- add seed to randoms so random always random
+  -- os.time() always increments so each load will be different
   math.randomseed(os.time())
 
   LargeFont = love.graphics.newFont('m3x6.ttf', 48)
@@ -30,12 +32,7 @@ function love.load()
   Xp2 = VIR_WIDTH - 15
   Yp2 = VIR_HEIGHT / 2 - 15
 
-
-  BallX = VIR_WIDTH / 2 - 2
-  BallY = VIR_HEIGHT / 2 - 2
-
-  BallDX = math.random(2) == 1 and 100 or -100
-  BallDY = math.random(-50, 50) * 1.5
+  InitBall()
 
   PaddleSpeed = 175
 
@@ -48,8 +45,14 @@ function DrawEntities()
   OpponentScore = 0
   love.graphics.clear(58 / 255, 45 / 255, 82 / 255, 1)
   love.graphics.setFont(LargeFont)
-  love.graphics.print(tostring(PlayerScore), VIR_WIDTH / 2 - 40, VIR_HEIGHT / 2 - 90)
-  love.graphics.print(tostring(OpponentScore), VIR_WIDTH / 2 + 30, VIR_HEIGHT / 2 - 90)
+  if GameState == 'play' then
+    love.graphics.print(tostring(PlayerScore), VIR_WIDTH / 2 - 40, VIR_HEIGHT / 2 - 90)
+    love.graphics.print(tostring(OpponentScore), VIR_WIDTH / 2 + 30, VIR_HEIGHT / 2 - 90)
+  else
+    love.graphics.setFont(SmallFont)
+    love.graphics.printf('Press "Enter" to Start Game', 0, 20, VIR_WIDTH, "center")
+  end
+
 
   --left paddle
   love.graphics.rectangle('fill', Xp1, Yp1, 5, 30)
@@ -61,6 +64,17 @@ function DrawEntities()
   love.graphics.rectangle('fill', BallX, BallY, 4, 4)
 end
 
+function InitBall()
+  BallX = VIR_WIDTH / 2 - 2
+  BallY = VIR_HEIGHT / 2 - 2
+
+  -- this randomizes the side either left(-100) or right(100) of where the ball is going to
+  BallDX = math.random(2) == 1 and 100 or -100
+
+  -- this randomizes the direction of the ball
+  BallDY = math.random(-50, 50) * 1.5
+end
+
 function love.keypressed(key)
   if key == 'escape' then
     love.event.quit()
@@ -69,6 +83,7 @@ function love.keypressed(key)
       GameState = 'play'
     else
       GameState = 'start'
+      InitBall()
     end
   end
 end
